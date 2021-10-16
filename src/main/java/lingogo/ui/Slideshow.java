ba9fcdfdae3cf9afcc482003778e1e834b5e0461
kwokieee <bryankwokyf@gmail.com>
2021-10-17 00:36:51 +0800

    Add Slideshow component

--- src/main/java/lingogo/ui/Slideshow.java
+++ src/main/java/lingogo/ui/Slideshow.java
@@ -2,8 +2,12 @@ package lingogo.ui;
 
 import java.util.logging.Logger;
 
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Region;
 import lingogo.commons.core.LogsCenter;
+import lingogo.model.flashcard.Flashcard;
 
 /**
  * A panel that displays flashcards when the user is testing themselves.
@@ -12,11 +16,29 @@ public class Slideshow extends UiPart<Region> {
     private static final String FXML = "Slideshow.fxml";
     private final Logger logger = LogsCenter.getLogger(FlashcardListPanel.class);
 
+    @FXML
+    private Label currentFlashcardNumber;
+    @FXML
+    private Label currentForeignPhrase;
+    @FXML
+    private Label progress;
+
+    ObservableList<Flashcard> flashcardList;
+
     /**
      * Creates a {@code Slideshow} component.
      */
-    public Slideshow() {
+    public Slideshow(ObservableList<Flashcard> flashcardList) {
         super(FXML);
+        this.flashcardList = flashcardList;
+    }
+
+    //TODO: Implement logic for displaying flashcards from the flashcardList one phrase at a time
+    public void beginSlideshow() {
+
+    }
+
+    public void endSlideshow() {
 
     }
 }
