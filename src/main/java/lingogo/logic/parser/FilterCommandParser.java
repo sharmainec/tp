f219210e3a5c9435daf67f0bd048faf485c79142
sharmaine <sharmainec@u.nus.edu>
2021-10-23 01:32:55 +0800

    include missing files

--- src/main/java/lingogo/logic/parser/FilterCommandParser.java
+++ src/main/java/lingogo/logic/parser/FilterCommandParser.java
@@ -2,12 +2,17 @@ package lingogo.logic.parser;
 
 import static java.util.Objects.requireNonNull;
 import static lingogo.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static lingogo.logic.parser.CliSyntax.PREFIX_INDEX_LIST;
 import static lingogo.logic.parser.CliSyntax.PREFIX_LANGUAGE_TYPE;
 
+import java.util.List;
+
+import lingogo.commons.core.index.Index;
 import lingogo.logic.commands.FilterCommand;
+import lingogo.logic.commands.FilterCommand.FilterBuilder;
 import lingogo.logic.parser.exceptions.ParseException;
-import lingogo.model.flashcard.LanguageTypeMatchesGivenPhrasePredicate;
 import lingogo.model.flashcard.Phrase;
+
 
 /**
  * Parses input arguments and creates a new FilterCommand object
@@ -23,14 +28,29 @@ public class FilterCommandParser implements Parser<FilterCommand> {
     @Override
     public FilterCommand parse(String args) throws ParseException {
         requireNonNull(args);
-        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_LANGUAGE_TYPE);
+        ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args, PREFIX_LANGUAGE_TYPE, PREFIX_INDEX_LIST);
 
-        if (!argMultimap.getValue(PREFIX_LANGUAGE_TYPE).isPresent()) {
+        FilterBuilder filterBuilder = new FilterBuilder();
+
+
+        if (argMultimap.getValue(PREFIX_LANGUAGE_TYPE).isPresent()) {
+            Phrase languageType = ParserUtil.parsePhrase(argMultimap.getValue(PREFIX_LANGUAGE_TYPE).get());
+            filterBuilder.setLanguageType(languageType);
+        }
+
+
+        if (argMultimap.getValue(PREFIX_INDEX_LIST).isPresent()) {
+            List<Index> indexList = ParserUtil.parseIndices(argMultimap.getValue(PREFIX_INDEX_LIST).get());
+            filterBuilder.setIndexList(indexList);
+        }
+
+        if (!filterBuilder.isAnyFieldFiltered()) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FilterCommand.MESSAGE_USAGE));
         }
 
-        Phrase languageType = ParserUtil.parsePhrase(argMultimap.getValue(PREFIX_LANGUAGE_TYPE).get());
 
-        return new FilterCommand(new LanguageTypeMatchesGivenPhrasePredicate(languageType));
+        return new FilterCommand(filterBuilder);
     }
+
+
 }
