fc95c6b1bb8736c6b3dd09fad9dfe96d3e43328c
kwokieee <bryankwokyf@gmail.com>
2021-10-23 00:34:15 +0800

    Update version number

--- src/main/java/lingogo/MainApp.java
+++ src/main/java/lingogo/MainApp.java
@@ -36,7 +36,7 @@ import lingogo.ui.UiManager;
  */
 public class MainApp extends Application {
 
-    public static final Version VERSION = new Version(1, 2, 0, false);
+    public static final Version VERSION = new Version(1, 3, 0, true);
 
     private static final Logger logger = LogsCenter.getLogger(MainApp.class);
 
