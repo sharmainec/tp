5a7ba2cdbf80b238e39075c6cd331fed2fd567b3
En Rong <53928333+chownces@users.noreply.github.com>
2021-11-06 16:07:34 +0800

    Fix export command result bug

--- src/main/java/lingogo/logic/commands/ExportCommand.java
+++ src/main/java/lingogo/logic/commands/ExportCommand.java
@@ -32,7 +32,7 @@ public class ExportCommand extends Command {
             getMessageUsage(COMMAND_WORD, COMMAND_DESCRIPTION, COMMAND_PARAMETERS, COMMAND_EXAMPLES);
 
     public static final String MESSAGE_SUCCESS =
-            "All existing flashcards have been saved in %1$s (located in the data folder).";
+            "The current displayed list of flashcards have been saved in %1$s (located in the data folder).";
 
     public static final String EXPORT_IOEXCEPTION = "Could not save flashcards from LingoGO! into %1$s";
 
